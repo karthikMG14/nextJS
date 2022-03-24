24b2777e565b293686378526fdc71d59233e1d2a
karthikMG14 <karthik.malliala@mind-graph.com>
2022-03-24 11:33:53 +0800

    updated page name

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Login Page</title>
+        <title>Login Pages</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
